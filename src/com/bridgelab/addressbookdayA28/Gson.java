9ee12512d3970004a8cf73aecd62481a3266df4c
Ameet Jadhav <ammugs11@gmail.com>
2022-10-08 10:10:32 +0530

    added use case 15 read and write into json

---
+++ src/com/bridgelab/addressbookdayA28/Gson.java
@@ -0,0 +1,18 @@
+package com.bridgelab.addressbookdayA28;
+
+import java.io.Reader;
+import java.util.ArrayList;
+
+public class Gson {
+
+	public String toJson(ArrayList<AddressBook> contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object fromJson(Reader reader, Class<AddressBook[]> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
